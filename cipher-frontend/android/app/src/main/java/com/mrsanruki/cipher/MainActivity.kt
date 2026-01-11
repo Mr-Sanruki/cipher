fb77a0c255b73ee58b92597eafd3f2242d3dadf4
Mr-Sanruki <mr.sunruki@gmail.com>
2026-01-12 05:21:26 +0530

    package fix

--- cipher-frontend/android/app/src/main/java/com/mrsanruki/cipher/MainActivity.kt
+++ cipher-frontend/android/app/src/main/java/com/mrsanruki/cipher/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rutikesh.cipher
+package com.mrsanruki.cipher
 
 import android.os.Build
 import android.os.Bundle
--- cipher-frontend/android/app/src/main/java/com/mrsanruki/cipher/MainApplication.kt
+++ cipher-frontend/android/app/src/main/java/com/mrsanruki/cipher/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rutikesh.cipher
+package com.mrsanruki.cipher
 
 import android.app.Application
 import android.content.res.Configuration
